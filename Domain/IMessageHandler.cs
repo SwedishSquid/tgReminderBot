1e9832c4814e870f702d0705ab0d9133959ee0b5
Alexey_Dzhevello <fishwere@mail.ru>
2024-01-12 18:08:00 +0500

    add imessageHandler

---
+++ Domain/IMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+
+namespace Domain
+{
+    public interface IMessageHandler
+    {
+        public bool TryHandleMessage(Message message, ITelegramBotClient bot);
+    }
+}
